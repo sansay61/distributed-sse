b23c610e7cc0e94ab766a8443f0c7ae3b9c4bf97
sanmaG <skhan@nurtelecom.kg>
2023-04-02 19:32:23 +0300

    initial commit

--- src/main/java/sergey/han/notification_service/exception/SseAuthenticationException.java
+++
@@ -1,4 +0,0 @@
-package sergey.han.notification_service.exception;
-
-public class SseAuthenticationException extends RuntimeException {
-}
--- src/main/java/sergey/han/notification_service/listener/RedisMessagePublisher.java
+++ src/main/java/sergey/han/notification_service/listener/RedisMessagePublisher.java
@@ -1,7 +1,6 @@
 package sergey.han.notification_service.listener;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -23,10 +22,10 @@ public class RedisMessagePublisher {
         redisTemplate.convertAndSend(topic.getTopic(), message);
     }
 
-    @Scheduled(fixedDelay = 5000)
+    /*@Scheduled(fixedDelay = 5000)
     private void heartbeat(){
         long i=0;
-        NotificationModel notificationModel=new NotificationModel().setMessage("hello"+(i++)).setEmitterId("32bd28cb-936a-4b11-a191-3924cbbc2276");
+        NotificationModel notificationModel=new NotificationModel().setMessage("heartbeat");
         publish(notificationModel);
-    }
+    }*/
 }
